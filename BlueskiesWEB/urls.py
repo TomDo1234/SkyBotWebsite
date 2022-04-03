c9f6cfc2f141383a233db2078f484a81f7715559
AloneInSky <90830966+AloneInSky@users.noreply.github.com>
2022-04-03 09:55:19 +0700

    Update urls.py

--- BlueskiesWEB/urls.py
+++ BlueskiesWEB/urls.py
@@ -1,4 +1,4 @@
-Ni"""BlueskiesWEB URL Configuration
+"""BlueskiesWEB URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('', include("home.urls")),
+    path('/', include("home.urls")),
     path('home/', include("home.urls")),
     path('suggest', include("suggest.urls")),
     path('admin/', admin.site.urls),
